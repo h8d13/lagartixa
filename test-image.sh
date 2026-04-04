d5fbc55f05132c077e849d3a7a514747af3e604b
h8d13 <hadrien.loge@gmail.com>
2026-04-05 01:13:01 +0200

    chore(cleanup)

--- test-image.sh
+++ test-image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Test klartix.sh against a loop device image no real disk needed
-###### 
+# Test klartix.sh against a loop device
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"

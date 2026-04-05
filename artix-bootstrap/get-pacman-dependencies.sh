6f31ecd038b4c4123d9dcf219f71c1627f621b74
h8d13 <hadean-eon-dev@proton.me>
2026-04-05 14:26:10 +0200

    chore(sc): continuation

--- artix-bootstrap/get-pacman-dependencies.sh
+++ artix-bootstrap/get-pacman-dependencies.sh
@@ -1,26 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2026 Artix
 set -e -u -o pipefail
 
 shared_dependencies() {
   local EXECUTABLE=$1
-  for PACKAGE in $(ldd "$EXECUTABLE" | grep "=> /" | awk '{print $3}'); do 
-    LC_ALL=c pacman -Qo $PACKAGE
+  for PACKAGE in $(ldd "$EXECUTABLE" | grep "=> /" | awk '{print $3}'); do
+    LC_ALL=c pacman -Qo "$PACKAGE"
   done | awk '{print $5}'
 }
 
 pkgbuild_dependencies() {
   local PKGBUILD=$1
   local EXCLUDE=$2
+  # shellcheck disable=SC1090
   source "$PKGBUILD"
-  for DEPEND in ${depends[@]}; do
+  # shellcheck disable=SC2154
+  for DEPEND in "${depends[@]}"; do
+    # shellcheck disable=SC2001
     echo "$DEPEND" | sed "s/[>=<].*$//"
   done | grep -v "$EXCLUDE"
 }
 
 # Main
-{ 
+{
   shared_dependencies "/usr/bin/pacman"
-  pkgbuild_dependencies "~/artools-workspace/artixlinux/packages/pacman/trunk/PKGBUILD" "bash"
+  pkgbuild_dependencies "$HOME/artools-workspace/artixlinux/packages/pacman/trunk/PKGBUILD" "bash"
 } | sort -u | xargs

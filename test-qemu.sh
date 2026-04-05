219fdddb4c629148f545f14d1361269c1bc4b9ab
h8d13 <hadrien.loge@gmail.com>
2026-04-05 02:17:22 +0200

    feat(vm): add qemu helper to test out img

---
+++ test-qemu.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# test-qemu.sh - boot artix image in QEMU UEFI
+# usage: ./test-qemu.sh [image]
+
+IMG="${1:-/tmp/artix-test.img}"
+OVMF_CODE="/usr/share/OVMF/OVMF_CODE.fd"
+OVMF_VARS="/tmp/ovmf_vars.fd"
+
+[ ! -f "$IMG" ] && { echo "Image not found: $IMG"; exit 1; }
+[ ! -f "$OVMF_VARS" ] && cp /usr/share/OVMF/OVMF_VARS.fd "$OVMF_VARS"
+
+qemu-system-x86_64 \
+    -enable-kvm \
+    -machine q35,accel=kvm \
+    -device intel-iommu \
+    -cpu host \
+    -global mc146818rtc.lost_tick_policy=slew \
+    -smp 2 \
+    -m 2G \
+    -drive if=pflash,format=raw,readonly=on,file="$OVMF_CODE" \
+    -drive if=pflash,format=raw,file="$OVMF_VARS" \
+    -drive file="$IMG",format=raw,if=virtio \
+    -nic user \
+    -display sdl,gl=on
